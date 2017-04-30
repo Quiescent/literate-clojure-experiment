47b73b5b0ff8f413d714d9e9dc2e1fecbb8f8def
Edward John Steere <edward.steere@gmail.com>
2017-04-30 17:09:31 +0200

    Adds work done in completing udemy course
     * Project setup: some Emacs Lisp to make it easier to work with in
       Emacs
     * Tange and Eval All: convenience code
     * Defining the Microservice: redefined to be much simpler
     * Routes: simplified and added more descriptions
     * Getting Projects from Github: added a section on how we can get
       details on projects from Github using the HTTP client
     * Services: added Basic service definitions used in the course
     * Database Queries: added a section on querying a mongo database for
       some project info
     * Pedestal Service Definition: simplified so that it can be tangled
       to one file
     * Web Server: changed the name spaces of service related functions
     * Testing the Service: Added a number of calls to the API which the
       micro service defines to show a user how it works

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.pedestal/pedestal.service "0.5.2"]
                  [io.pedestal/pedestal.jetty "0.5.2"]
-                 [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.22"]
-                 [org.slf4j/jcl-over-slf4j "1.7.22"]
-                 [org.slf4j/log4j-over-slf4j "1.7.22"]]
+                 [com.novemberain/monger "3.1.0"]
+                 [clj-http "3.4.1"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.xml "0.0.8"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"])
