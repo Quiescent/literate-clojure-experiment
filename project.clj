9c0b9de493211f792de23fa15ceee72b56f4adef
Edward John Steere <edward.steere@gmail.com>
2017-05-09 20:51:27 +0200

    Fixes minor problems discovered while practicing
     - presentation/merge.html updated after tangling again
     - presentation/slides.org:"Story Time" added note for duration of
       talk
     - project-catalogue.org:"Starting the Microservice" added a sit for
       to wait for jetty to start before running dev
     - project-catalogue.org:"Searching Github" updated the list of
       projects from Github
     - project.clj: added back the logging dependencies

--- project.clj
+++ project.clj
@@ -9,6 +9,10 @@
                  [com.novemberain/monger "3.1.0"]
                  [clj-http "3.4.1"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/data.xml "0.0.8"]]
+                 [org.clojure/data.xml "0.0.8"]
+                 [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.22"]
+                 [org.slf4j/jcl-over-slf4j "1.7.22"]
+                 [org.slf4j/log4j-over-slf4j "1.7.22"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"])
